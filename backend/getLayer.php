901252b80e4b109b08e0fb5f2ea33231c56f1f95
bmearns <mearns@udel.edu>
2014-07-14 13:53:04 -0700

    fixed i/o disk error on getlayer

--- backend/getLayer.php
+++ backend/getLayer.php
@@ -12,28 +12,54 @@ SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.geometry
   GROUP BY b.STATE_ABBR; */
   
 $strParam='WIND SPEED';
-$wktBounding='POLYGON((-75.8 38.4, -75.0 38.4, -75.0 39.85, -75.8 39.85))';
+$wktBounding='POLYGON((-75.8 38.4, -75.0 38.4, -75.0 39.85, -75.8 39.85, -75.8 38.4))';
 $dateMin='042009';
 $dateMax='092009';
+$query1="CREATE TABLE IF NOT EXISTS states_bounding AS
+SELECT * FROM states WHERE ST_Intersects(geometry, PolyFromText('$wktBounding',4236))=1";
+$query="SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.geometry
+                                        FROM  measurement AS a, 
+                                        (SELECT STATE_ABBR, geometry FROM states_bounding)  AS b
+                                        WHERE ST_Intersects(a.geometry,b.geometry)=1
+                                        AND a.parameter = '$strParam'
+                                        AND date(a.date_measured) < date($dateMin)
+                                        AND date(a.date_measured) > date($dateMax)
+                                        GROUP BY b.STATE_ABBR;";
   
 try {
 	
-	function getLayer ($strParam,$wktBounding,$dateMin,$dateMax) {
+	function getLayer ($strParam,$wktBounding,$dateMin,$dateMax,$query,$query1) {
 		$db = new SQLite3('limabean.sqlite');
-		
+		//$db->busyTimeout(80000);
 		//loading spatialite extension
 		$db->loadExtension('libspatialite.so.2');
-		
-		
+	
+	
+	/*	$query = "SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.geometry
+                                        FROM  measurement AS a, 
+                                        (SELECT STATE_ABBR, geometry FROM states) AS b
+                                        WHERE ST_Intersects(a.geometry,b.geometry)=1
+                                        AND a.parameter = '$strParam'
+                                        AND date(a.date_measured) < date($dateMin)
+                                        AND date(a.date_measured) > date($dateMax)
+                                        GROUP BY b.STATE_ABBR;";
+	
 		$query = "SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.geometry
 					FROM  measurement AS a, 
-					(SELECT STATE_ABBR, geometry FROM states WHERE ST_Intersects(geometry, PolyFromText($wktBounding,4236))=1) AS b
+					(SELECT STATE_ABBR, geometry FROM states WHERE ST_Intersects(geometry, PolyFromText('$wktBounding',4236))=1) AS b
 					WHERE ST_Intersects(a.geometry,b.geometry)=1
 					AND a.parameter = '$strParam'
 					AND date(a.date_measured) < date($dateMin)
 					AND date(a.date_measured) > date($dateMax)
-					GROUP BY b.STATE_ABBR;";
+					GROUP BY b.STATE_ABBR;"; */
 		echo $query;
+
+		$st = $db->query($query1);
+                if (! $st) {
+                        $error = $db->errorInfo();
+                        print "Problem ({$error[2]})";
+
+                }
 		
 		$st = $db->query($query);
 		if (! $st) {
@@ -41,6 +67,7 @@ try {
 			print "Problem ({$error[2]})";
 		
 		}
+		
 		while ($res = $st->fetchArray(SQLITE3_ASSOC)){
 			foreach ($res as $key => $value) {
 				echo $key . ' = ' . $value . ",\n";
@@ -54,7 +81,7 @@ try {
     die();
 }
 
-getLayer($strParam,$wktBounding,$dateMin,$dateMax);
+getLayer($strParam,$wktBounding,$dateMin,$dateMax,$query,$query1);
 //echo 'hello world';
 
 ?>
