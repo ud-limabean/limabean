e2e1231e18cbbd81e6df533f039cb0addb98ac3a
bmearns <mearns@udel.edu>
2014-07-16 08:45:00 -0700

    fix query in dolayer

--- backend/getLayer.php
+++ backend/getLayer.php
@@ -1,26 +1,19 @@
 <?php
 //phpinfo();
 //exit();
-  
+ 
+/* NEED TO HAVE THESE PARAMETERS PASSED, ELSE FALL BACK TO DEFAULTS (THESE FOR NOW) */ 
 $strParam='WIND SPEED';
 $wktBounding='POLYGON((-75.8 38.4, -75.0 38.4, -75.0 39.85, -75.8 39.85, -75.8 38.4))';
 $dateMin='042009';
 $dateMax='092009';
-/* $query1="CREATE TABLE IF NOT EXISTS states_bounding AS
-SELECT * FROM states WHERE ST_Intersects(geometry, PolyFromText('$wktBounding',4236))=1";
-$query="SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.geometry
-                                        FROM  measurement AS a, 
-                                        (SELECT STATE_ABBR, geometry FROM states_bounding)  AS b
-                                        WHERE ST_Intersects(a.geometry,b.geometry)=1
-                                        AND a.parameter = '$strParam'
-                                        AND date(a.date_measured) < date($dateMin)
-                                        AND date(a.date_measured) > date($dateMax)
-                                        GROUP BY b.STATE_ABBR;"; */
 										
 $query="SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.geometry
                                         FROM  measurement AS a, 
                                         (SELECT STATE_ABBR, geometry FROM states)  AS b
-                                        WHERE ST_Intersects(a.geometry, PolyFromText('$wktBounding',4236))=1
+                                        WHERE
+					a.state_abbr=b.STATE_ABBR
+					AND ST_Intersects(b.geometry, PolyFromText('$wktBounding',4326))=1
                                         AND a.parameter = '$strParam'
                                         AND date(a.date_measured) < date($dateMin)
                                         AND date(a.date_measured) > date($dateMax)
@@ -28,41 +21,13 @@ $query="SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.
   
 try {
 	
-	function getLayer ($strParam,$wktBounding,$dateMin,$dateMax,$query,$query1) {
-		echo $query;
-		exit();
+	function getLayer ($strParam,$wktBounding,$dateMin,$dateMax,$query) {
 		$db = new SQLite3('limabean.sqlite');
 		//$db->busyTimeout(80000);
 		//loading spatialite extension
 		$db->loadExtension('libspatialite.so.2');
 	
 	
-	/*	$query = "SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.geometry
-                                        FROM  measurement AS a, 
-                                        (SELECT STATE_ABBR, geometry FROM states) AS b
-                                        WHERE ST_Intersects(a.geometry,b.geometry)=1
-                                        AND a.parameter = '$strParam'
-                                        AND date(a.date_measured) < date($dateMin)
-                                        AND date(a.date_measured) > date($dateMax)
-                                        GROUP BY b.STATE_ABBR;";
-	
-		$query = "SELECT Avg(a.value) AS VALUE,a.date_measured,a.parameter,b.STATE_ABBR,b.geometry
-					FROM  measurement AS a, 
-					(SELECT STATE_ABBR, geometry FROM states WHERE ST_Intersects(geometry, PolyFromText('$wktBounding',4236))=1) AS b
-					WHERE ST_Intersects(a.geometry,b.geometry)=1
-					AND a.parameter = '$strParam'
-					AND date(a.date_measured) < date($dateMin)
-					AND date(a.date_measured) > date($dateMax)
-					GROUP BY b.STATE_ABBR;"; */
-		echo $query;
-
-		$st = $db->query($query1);
-                if (! $st) {
-                        $error = $db->errorInfo();
-                        print "Problem ({$error[2]})";
-
-                }
-		
 		$st = $db->query($query);
 		if (! $st) {
 			$error = $db->errorInfo();
@@ -83,7 +48,7 @@ try {
     die();
 }
 
-getLayer($strParam,$wktBounding,$dateMin,$dateMax,$query,$query1);
+getLayer($strParam,$wktBounding,$dateMin,$dateMax,$query);
 //echo 'hello world';
 
 ?>
