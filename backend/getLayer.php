eafbbb1c0bc37eb84b07f5c4994b8e132ae68229
Ben Mearns <mearns@css-rdms6.(none)>
2014-09-12 14:58:43 -0400

    removed bounding box query

--- backend/getLayer.php
+++ backend/getLayer.php
@@ -2,12 +2,10 @@
 
 /* NEED TO HAVE THESE PARAMETERS PASSED, ELSE FALL BACK TO DEFAULTS (THESE FOR NOW) */ 
 $strParam='WIND SPEED';
-$strBounding='-86.484375,35.209721645221386,-70.13671875,41.409775832009565';
 $dateMin='2009-04-01';
 $dateMax='2009-09-01';
 
 if (isset($_REQUEST['param'])){$strParam=$_REQUEST['param'];}
-if (isset($_REQUEST['bounding'])){$strBounding=$_REQUEST['bounding'];}
 if (isset($_REQUEST['min'])){$dateMin=$_REQUEST['min'];}
 if (isset($_REQUEST['max'])){$dateMax=$_REQUEST['max'];}
 
@@ -17,14 +15,13 @@ $query="SELECT Avg(a.value) AS value, a.date_measured as date,a.parameter as par
                                         (SELECT STATE_ABBR, geometry, AsGeoJSON(geometry) as jGeom FROM states)  AS b
                                         WHERE
 					a.state_abbr=b.STATE_ABBR
-					AND ST_Intersects(b.geometry, BuildMbr($strBounding))=1
                                         AND a.parameter = '$strParam'
                                         AND date(a.date_measured) BETWEEN date('$dateMin') AND date('$dateMax')
                                         GROUP BY b.STATE_ABBR;";
   
 try {
 	
-	function getLayer ($strParam,$strBounding,$dateMin,$dateMax,$query) {
+	function getLayer ($strParam,$dateMin,$dateMax,$query) {
 		$db = new SQLite3('limabean.sqlite');
 		//$db->busyTimeout(80000);
 		//loading spatialite extension
@@ -86,6 +83,6 @@ try {
     die();
 }
 
-getLayer($strParam,$strBounding,$dateMin,$dateMax,$query);
+getLayer($strParam,$dateMin,$dateMax,$query);
 
 ?>
