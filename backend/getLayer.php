8e944d4934b426ccc47d45777126ecf9ececc58e
bmearns <mearns@udel.edu>
2014-10-14 16:15:03 -0400

    progress on mysqli

--- backend/getLayer.php
+++ backend/getLayer.php
@@ -10,28 +10,37 @@ if (isset($_REQUEST['min'])){$dateMin=$_REQUEST['min'];}
 if (isset($_REQUEST['max'])){$dateMax=$_REQUEST['max'];}
 
 //could be improved by using bounding box instead of intersection on polygon										
-$query="SELECT Avg(a.value) AS value, a.date_measured as date,a.parameter as param,b.STATE_ABBR as state,b.jGeom as geometry
+/* $query="SELECT Avg(a.value) AS value, a.date_measured as date,a.parameter as param,b.STATE_ABBR as state,b.jGeom as geometry
                                         FROM  measurement AS a, 
                                         (SELECT STATE_ABBR, geometry, AsGeoJSON(geometry) as jGeom FROM states)  AS b
                                         WHERE
 					a.state_abbr=b.STATE_ABBR
                                         AND a.parameter = '$strParam'
                                         AND date(a.date_measured) BETWEEN date('$dateMin') AND date('$dateMax')
-                                        GROUP BY b.STATE_ABBR;";
-  
+                                        GROUP BY b.STATE_ABBR;"; */
+										
+$query="select value, tom as date, parameter as param 
+		FROM v_measurement 
+        WHERE
+		parameter = '$strParam'
+        AND date(tom) BETWEEN date('$dateMin') AND date('$dateMax');";
+
+//adapted for mysql, geojson is for points, can construct in parser										
 try {
 	
 	function getLayer ($strParam,$dateMin,$dateMax,$query) {
-		$db = new SQLite3('limabean.sqlite');
+		//$db = new SQLite3('limabean.sqlite');
 		//$db->busyTimeout(80000);
 		//loading spatialite extension
-		$db->loadExtension('libspatialite.so');
+		//$db->loadExtension('libspatialite.so');
+		
+		$db = new mysqli("68.178.143.16", "bbagbv2pilot", "B!tt!rB!an12", "bbagbv2pilot");
+		//sh mysql2sqlite.sh -u  --host=68.178.143.16 --password='B!tt!rB!an12'  bbagbv2pilot --tables div_locality div_measurement | sqlite3 limabean2.sqlite
 	
 		//echo $query;
-		$st = $db->query($query);
-		if (! $st) {
-			$error = $db->errorInfo();
-			print "Problem ({$error[2]})";
+		$result = $db->query($query);
+		if (! $result) {
+			printf("Errormessage: %s\n", $db->error);
 
 		}
 	/*
@@ -50,6 +59,52 @@ try {
 			$data['features'][]=$record;
 		}
 	*/
+
+		# Build GeoJSON
+		$output = '';
+		$rowOutput = '';
+ 
+		while ($row = $result->fetch_assoc()) {
+    			$rowOutput = (strlen($rowOutput) > 0 ? ',' : '') . '{"type": "Feature", "geometry": ' . $row['geometry'] . ', "properties": {';
+    			$props = '';
+    			$id = '';
+    			foreach ($row as $key => $val) {
+        			if ($key != "geometry") {
+            				$props .= (strlen($props) > 0 ? ',' : '') . '"' . $key . '":"' . $val . '"';
+        			}
+    			}
+    
+    			$rowOutput .= $props . '}';
+    			$rowOutput .= '}';
+    			$output .= $rowOutput;
+		}
+ 
+		$output = '{ "type": "FeatureCollection", "features": [ ' . $output . ' ]}';
+
+
+		header('Content-Type: application/json');
+		echo $output;
+		}
+	
+    $dbh = null;
+} catch (Exception $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+// Block below was on sqlite										
+/* try {
+	
+	function getLayer ($strParam,$dateMin,$dateMax,$query) {
+		$db = new SQLite3('limabean.sqlite');
+		$db->loadExtension('libspatialite.so');
+	
+		$st = $db->query($query);
+		if (! $st) {
+			$error = $db->errorInfo();
+			print "Problem ({$error[2]})";
+
+		}
 
 		# Build GeoJSON
 		$output = '';
@@ -81,7 +136,7 @@ try {
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
-}
+} */
 
 getLayer($strParam,$dateMin,$dateMax,$query);
 
