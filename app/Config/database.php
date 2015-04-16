1b25b8747e71e0e50e4bb26a54db3a8563e7d7ec
bmearns <mearns@udel.edu>
2015-04-16 10:59:33 -0400

    before pull

---
+++ app/Config/database.php
@@ -0,0 +1,31 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '128.175.68.212',
+		'port' => '3306',
+		'login' => 'limabean',
+		'password' => 'limabean',
+		'database' => 'bbagbv2pilot',
+		'prefix' => 'div_',
+		//'encoding' => 'utf8',
+	);
+}
+
+/*
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '68.178.143.16',
+		'login' => 'bbagbv2pilot',
+		'password' => 'B!tt!rB!an12',
+		'database' => 'bbagbv2pilot',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+}*/
